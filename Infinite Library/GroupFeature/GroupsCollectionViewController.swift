46a39153954d9383108dd0d747f9394b3561768d
Harry Summers <hsummers96@gmail.com>
2018-06-12 13:57:39 +0300

    Changed data model

---
+++ Infinite Library/GroupFeature/GroupsCollectionViewController.swift
@@ -0,0 +1,9 @@
+//
+//  GroupsCollectionViewController.swift
+//  InfiniteLibrary
+//
+//  Created by Harry Summers on 6/12/18.
+//  Copyright © 2018 harrysummers. All rights reserved.
+//
+
+import Foundation
